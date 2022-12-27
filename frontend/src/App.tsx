30374b497f887aec8f16b0f66c1b2ac79b306055
Bruno Miguel Alves Costa <brunomiguelalvescosta@Bruno-Costas-MacBook.local>
2022-12-27 18:07:28 +0000

    Add components folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
+import ExpenseList from './components/ExpenseList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
+      <ExpenseList />
     </div>
   );
 }
