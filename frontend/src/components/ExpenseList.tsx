4232688de890e43278709d15ba3fb4593dd64011
Bruno Miguel Alves Costa <brunomiguelalvescosta@Bruno-Costas-MacBook.local>
2022-12-27 18:15:21 +0000

    Add export to Expense interface, import Expense interface to ExpenseList

--- frontend/src/components/ExpenseList.tsx
+++ frontend/src/components/ExpenseList.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import {Expense} from '../../../api/data/expenses';
 
 const ExpenseList = () => {
+
+    
+
   return (
     <div>
         <h1>Expense List</h1>
+        
     </div>
   )
 }
